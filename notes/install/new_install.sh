86a505bc9b052f6e530e623a68d7dc02d3d7684a
Chris Ching <chris@ching.codes>
2018-10-13 15:19:36 -0600

    Update submodules
    * Update all submodules
    * Dropped oh-my-zsh
    * Update Vundle to new path
    * Change new_install checkout all repos submodules

--- notes/install/new_install.sh
+++ notes/install/new_install.sh
@@ -2,14 +2,8 @@
 
 set -x
 
-if [ ! -z "$VCSH_REPO_NAME" ]; then
-	echo Make sure you are in a vcsh configured shell
-	exit 1
-fi
-
 echo Cloning submodules
-git submodule update --recursive --init
-
+vcsh foreach submodule update --recursive --init
 
 # Configure oh-my-fish if fish is installed
 if which fish > /dev/null ; then
